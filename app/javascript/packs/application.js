950c590f3c6ff43897f2c80501354fa0aa1941a9
Iram Munir <irammunir786444@gmail.com>
2024-02-05 14:10:14 +0500

    Devise initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,11 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+
+global.$ = global.JQuery = require('jquery');
+global.bootstrap = require('bootstrap');
+global.Popper    = require('popper.js');
+require('bootstrap/dist/js/bootstrap');
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
