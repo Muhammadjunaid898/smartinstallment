aba0f3e8e9f4f54d4c8a35a695c9066031e026b2
Iram Munir <irammunir786444@gmail.com>
2024-02-17 14:57:10 +0500

    Creation date removed

--- db/migrate/20240217082653_create_companies.rb
+++ db/migrate/20240217082653_create_companies.rb
@@ -3,7 +3,6 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
     create_table :companies do |t|
       t.string :name
       t.string :subdomain
-      t.date :creation_date
       t.integer :admin_id
 
       t.timestamps
