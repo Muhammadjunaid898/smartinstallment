584097d6c4a9b0843fac5d9b06be40f3c617b682
junaid javed <junaidjaved898@gmail.com>
2024-02-22 00:41:20 +0500

    Add fields to signUp from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     view_context.dashboard_path
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     view_context.root_url
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :sub_domain])
   end
 end
