be8707a46d06357b4cfdd0f3667a603748352afd
Junaid Javed <41578511+Muhammadjunaid898@users.noreply.github.com>
2024-03-13 21:31:13 +0500

    implement multitenancy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index", via: :get
+  constraints subdomain: /.+/ do 
   get 'dashboard', to: "companies#dashboard"
+  end
 end
