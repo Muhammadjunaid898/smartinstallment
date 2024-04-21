ba057ab11ed2f6f9322f8eea3346a6da383672d5
Iram Munir <irammunir786444@gmail.com>
2024-04-21 12:53:07 +0500

    error_handling_branch

--- app/controllers/catch_all_controller.rb
+++ app/controllers/catch_all_controller.rb
@@ -1,8 +1,6 @@
 class CatchAllController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
 
-  rescue_from Exception, :with => :render_404
-
   def index
     InvalidRoute.create(url: request.url, relative_path: request.fullpath, request_type: request.request_method)
     render_404
